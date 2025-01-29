51f8b1eda5fa0288fc895d227bbe3649a94c007c
patelharish <patelharish304@gmail.com>
2025-01-29 09:54:37 +0530

    login

--- cypress/e2e/tests/loginTest.cy.js
+++ cypress/e2e/tests/loginTest.cy.js
@@ -14,6 +14,5 @@ describe(' test automation', () => {
         loginObj.enterPassword(loginData.password)
         loginObj.clickOnSubmit() 
         loginObj.clickOnAfterLoginBtn()
-
    })
 })        
