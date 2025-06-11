8a3d924c9caf0ee95e8c1a61fb6da1592d51a0ca
patelharish <patelharish304@gmail.com>
2025-06-11 16:12:34 +0530

    updated yml file

--- cypress/e2e/tests/addToCartTest.cy.js
+++ cypress/e2e/tests/addToCartTest.cy.js
@@ -12,12 +12,17 @@ describe(' test automation', () => {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115 Safari/537.36'
             }
         });
+        cy.wait(2000);
         //addToCartObj.clickOnAllowNtfcn()  
         addToCartObj.clickOnViewAllBtn()
+         cy.wait(2000);
         addToCartObj.clickOnFirstProduct()
+         cy.wait(2000);
         addToCartObj.selectOnSize()
+         cy.wait(2000);
         // addToCartObj.verifyProductName().should('contain', loginData.product.productName)
         addToCartObj.clickOnAddToCartBtn()
+         cy.wait(2000);
         // addToCartObj.clickOnCartIcon()
     })
 })
--- cypress/pages/addToCartPage.js
+++ cypress/pages/addToCartPage.js
@@ -16,19 +16,15 @@ export class addToCartPage{
     } 
     clickOnViewAllBtn(){
         cy.contains(this.weblocators.viewBtn).click()
-        cy.wait(3000);
     }
     clickOnFirstProduct(){
         cy.get(this.weblocators.firstProduct).first().click()
-        cy.wait(3000);
     }
     selectOnSize(){
         cy.get(this.weblocators.selectSize).click();
-        cy.wait(3000);
     }
     clickOnAddToCartBtn(){
         cy.get(this.weblocators.addToCartBtn).click()
-        cy.wait(3000);
     }
     /*clickOnCartIcon(){
         cy.get(this.weblocators.cartIcon).click()
