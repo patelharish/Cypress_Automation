bfd0e407bfed7a050f98fd2a08eaf49068ddc117
patelharish <patelharish304@gmail.com>
2025-06-11 15:45:35 +0530

    updated yml

--- cypress/e2e/tests/addToCartTest.cy.js
+++ cypress/e2e/tests/addToCartTest.cy.js
@@ -4,15 +4,20 @@ import loginData from '../../fixtures/loginData.json'
 
 
 describe(' test automation', () => {
-    
+
     it('add to cart flow', () => {
-        addToCartObj.openURL({ failOnStatusCode: false })  
+        addToCartObj.openURL({
+            failOnStatusCode: false,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115 Safari/537.36'
+            }
+        });
         //addToCartObj.clickOnAllowNtfcn()  
         addToCartObj.clickOnViewAllBtn()
         addToCartObj.clickOnFirstProduct()
         addToCartObj.selectOnSize()
-       // addToCartObj.verifyProductName().should('contain', loginData.product.productName)
+        // addToCartObj.verifyProductName().should('contain', loginData.product.productName)
         addToCartObj.clickOnAddToCartBtn()
-       // addToCartObj.clickOnCartIcon()
-   })
+        // addToCartObj.clickOnCartIcon()
+    })
 })
